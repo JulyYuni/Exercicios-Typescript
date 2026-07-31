a6506da0dbae70a870813a9072c6611cae4b7729
Julia <jujucc_2003@hotmail.com>
2026-07-30 22:01:53 -0300

    Exercicio 3

---
+++ exercicio3.ts
@@ -0,0 +1,17 @@
+interface Livro {
+  titulo: string;
+  autor: string;
+  anoPublicacao: number;
+}
+
+function resumirLivro(livro: Livro) {
+  return `O livro '${livro.titulo}' foi escrito por ${livro.autor} em ${livro.anoPublicacao}.`;
+}
+
+const livro: Livro = {
+  titulo: "Dom Casmurro",
+  autor: "Machado de Assis",
+  anoPublicacao: 1899,
+};
+
+console.log(resumirLivro(livro));
