522fb7a693a63005c53748a63fe913a2a2d4224f
Julia <jujucc_2003@hotmail.com>
2026-07-30 23:00:41 -0300

    Removendo arquivo

--- exercicio1.js
+++
@@ -1,9 +0,0 @@
-var total = 100;
-function adicionar(a, b) {
-    if (a === void 0) { a = 0; }
-    if (b === void 0) { b = 0; }
-    total = a + b;
-    return total;
-}
-var resultado = adicionar(20, 30);
-console.log(resultado); // 50
